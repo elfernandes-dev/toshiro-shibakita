a7de2d51ba02b1c1f821c5cc70a73d5e5c791a2c
Evandro Fernandes <elfernandes.dev@gmail.com>
2023-02-28 12:47:09 -0300

    Arquivos README.md,  banco.sql e index.php atualizados

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dados (
-    AlunoID int,
-    Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
-    Host varchar(50)
+    AlunoID INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Nome VARCHAR(50) NOT NULL,
+    Sobrenome VARCHAR(50) NOT NULL,
+    Endereco VARCHAR(150) NOT NULL,
+    Cidade VARCHAR(50) NOT NULL,
+    Host VARCHAR(50) NOT NULL
 );
